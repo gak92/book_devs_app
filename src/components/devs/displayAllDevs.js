e2d2cc0ab106c3f3189fb96d3d3e32f53c092b7f
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-12-06 14:03:01 +0100

    Create components

---
+++ src/components/devs/displayAllDevs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DisplayAllDevs() {
+  return (
+    <div>
+      <h1>Our Developers</h1>
+      <h3>Please select one to reserve a meeting</h3>
+    </div>
+  );
+}
+
+export default DisplayAllDevs;
