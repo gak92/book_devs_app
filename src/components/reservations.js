cc250a85c7fc0c90fb4a7a7992dd477a8eaba76b
Owusu-Desmond <desmond0406114009@gmail.com>
2022-12-06 10:52:50 +0000

    Create components

---
+++ src/components/reservations.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchReservations } from "../store/reservations/reservations";
+
+const Reservations = () => {
+
+    const dispatch = useDispatch();
+    const reservations = useSelector((state) => state.reservations.reservations);
+
+    useEffect(() => {
+        dispatch(fetchReservations());
+    }, []);
+
+    const convertDate = (date) => {
+        const newDate = new Date(date);
+        return newDate.toDateString();
+    };
+
+    console.log(reservations);
+    return (
+      // list of reservations
+      <div>
+        <h1>Reservations</h1>
+        <ul>
+          {reservations.map((reservation) => (
+            <li key={reservation.id}>
+              <h2>{reservation.name}</h2>
+              <p>{reservation.city}</p>
+              <p>{convertDate(reservation.reservation_date)}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    );
+}
+
+export default Reservations;
---
+++ src/components/sideNav.js
@@ -0,0 +1,35 @@
+// @flow
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+const Sidenav = () => {
+  return (
+    <nav className="sidenav">
+        <ul>
+            <li>
+                <NavLink to="/" >
+                    Developers
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/adddeveloper">
+                    Add Developer
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/reservations">
+                    My Reservations
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/deletedeveloper">
+                    Delete Developer
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Sidenav;
