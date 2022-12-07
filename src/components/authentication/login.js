90667a519bcca76b9551995fbeecb633400d976e
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-07 23:53:03 +0500

    Add functionality of user login on submit login form

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { postLoginDetails } from '../../store/user/auth';
+
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.authentication);
+
+  // console.log("user: ", user);
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    if(email === '' || password === '') return
+
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+    dispatch(postLoginDetails({email, password}));
+    setEmail('');
+    setPassword('');
+  };
+
+  if(user.loggedIn) {
+    return <Navigate replace to="/" />;
+  }
+
   return (
-    <section className="Auth-form-container">
+    <section className="Auth-form-container" onSubmit={loginUser}>
       <form className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
@@ -13,6 +39,9 @@ const Login = () => {
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           
@@ -22,6 +51,9 @@ const Login = () => {
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           
