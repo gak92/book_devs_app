e64b8cdaf258e98646ae2f8dda1fe108eb247ff3
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-07 17:46:05 +0500

    Add login form

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-function Login() {
+const Login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <section className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Login;
