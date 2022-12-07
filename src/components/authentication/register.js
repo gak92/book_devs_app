d91a663fb8e34ccc02a4136a6a3934edf08de46d
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-07 17:46:33 +0500

    Add form for sign up / registration

--- src/components/authentication/register.js
+++ src/components/authentication/register.js
@@ -1,9 +1,56 @@
 import React from 'react';
 
-function Register() {
+const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <section className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary">
+              Sign In
+            </span>
+          </div>
+          
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Register;
