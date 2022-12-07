cb0056f0dfb56a49cbdedaabf4e53b1424701061
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-08 00:41:22 +0500

    Update sideNav to show logout link if user loggedIn

--- src/components/sideNav.js
+++ src/components/sideNav.js
@@ -1,9 +1,14 @@
 // @flow
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { userLogout } from '../store/user/auth';
 
 
 const Sidenav = () => {
+	const authuser = localStorage.getItem('userAuth');
+	const dispatch = useDispatch();
+
   return (
     <nav className="sidenav">
         <ul>
@@ -12,16 +17,26 @@ const Sidenav = () => {
                     Developers
                 </NavLink>
             </li>
-            <li>
-                <NavLink to="/login" >
-                    Login
+						{authuser ? 
+							(<li>
+                <NavLink to="/login" onClick={() => dispatch(userLogout(authuser))} >
+                    Logout
                 </NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" >
-                    Register
-                </NavLink>
-            </li>
+            	</li>) 
+							: 
+							(<>
+								<li>
+									<NavLink to="/login" >
+											Login
+									</NavLink>
+								</li>
+								<li>
+									<NavLink to="/register" >
+										Register
+									</NavLink>
+								</li>
+							</>)
+						}
             <li>
                 <NavLink to="/adddeveloper">
                     Add Developer
