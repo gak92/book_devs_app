fd4ce03a86ed8db1cd5d7d70271d7dba2b5c4ddb
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-07 23:51:20 +0500

    Update configureStore by adding authentication reducer

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import reservationsReducer from "./reservations/reservations";
+import authReducer from "./user/auth";
 
 const rootReducer = combineReducers({
     reservations: reservationsReducer,
+    authentication: authReducer,
 });
 
 const store = configureStore({
