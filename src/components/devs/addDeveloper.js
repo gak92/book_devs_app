18930af211fba7de5f5c4bedf53e75fb6b889bf0
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-12-13 14:29:03 +0100

    Add developer function

--- src/components/devs/addDeveloper.js
+++ src/components/devs/addDeveloper.js
@@ -1,52 +1,114 @@
-import React from 'react';
-import './addDeveloper.css';
+import React, { useState, useEffect } from "react";
+import "./addDeveloper.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addDev } from "../../store/developers/devs/devActions";
 
 function AddDeveloper() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState([]);
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [salary, setSalary] = useState("");
+  const [rating, setRating] = useState("");
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    if (name && image && title && description && salary && rating) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [name, image, title, description, salary, rating]);
+
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
+
+  const submitHandle = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("image", image);
+    formData.append("description", description);
+    formData.append("title", title);
+    formData.append("salary_exp", salary);
+    formData.append("rating", rating);
+    formData.append("user_id", 1);
+
+    dispatch(addDev(formData));
+    Navigate("/developers");
+  };
+
+  // handle image upload
+  const handleUploadImage = (e) => {
+    setImage(e.target.files[0]);
+  };
+
   return (
-    <form className="addDeveloper">
+    <form onSubmit={submitHandle} className="addDeveloper">
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Developer Name
-        </label>
-        <input type="text" className="form-control" id="name" />
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          value={name}
+          placeholder="Developer Name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className="mb-3">
-        <label htmlFor="photo" className="form-label">
-          Developer Picture
-        </label>
-        <input type="text" className="form-control" id="photo" />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Developer Description
-        </label>
-        <input type="text" className="form-control" id="description" />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Developer Title
-        </label>
-        <input type="text" className="form-control" id="title" />
+        <input
+          type="file"
+          className="form-control"
+          id="photo"
+          placeholder="Developer Picture"
+          onChange={handleUploadImage}
+          multiple
+        />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="salary" className="form-label">
-          Developer Salary
-        </label>
-        <input type="number" className="form-control" id="salary" />
+        <input
+          type="text"
+          className="form-control"
+          id="description"
+          placeholder="Developer Description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="rating" className="form-label">
-          Developer Rating
-        </label>
-        <input type="number" className="form-control" id="rating" />
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Developer Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="number"
+          className="form-control"
+          id="salary"
+          placeholder="Developer Salary"
+          onChange={(e) => setSalary(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <input
+          type="number"
+          className="form-control"
+          id="rating"
+          placeholder="Developer Rating"
+          onChange={(e) => setRating(e.target.value)}
+        />
       </div>
 
       <button type="submit" className="btn btn-primary">
-        Submit
+        Add Developer
       </button>
     </form>
   );
--- src/store/developers/devs/devActions.js
+++ src/store/developers/devs/devActions.js
@@ -1,13 +1,64 @@
-import * as types from './devTypes';
+// import * as types from './devTypes';
 
-const getDevelopers = (developers) => ({
-  type: types.GET_DEVELOPERS_SUCCESS,
-  payload: developers,
-});
+// const getDevelopers = (developers) => ({
+//   type: types.GET_DEVELOPERS_SUCCESS,
+//   payload: developers,
+// });
 
-const addDeveloper = (developer) => ({
-  type: types.ADD_DEVELOPER,
-  payload: developer,
-});
+// const addDeveloper = (developer) => ({
+//   type: types.ADD_DEVELOPER,
+//   payload: developer,
+// });
 
-export { getDevelopers, addDeveloper };
+// export { getDevelopers, addDeveloper };
+
+import axios from "axios";
+import * as types from "./devTypes";
+
+const getDevelopers = () => async (dispatch) => {
+  try {
+    dispatch({ type: types.GET_DEVELOPERS_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    };
+    const { data } = await axios.get(
+      "http://127.0.0.1:3000/api/v1/developers",
+      config
+    );
+    dispatch({
+      type: types.GET_DEVELOPERS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: types.GET_DEVELOPERS_FAIL,
+      payload:
+        error.response && error.response.data.error
+          ? error.response.data.error
+          : error.error,
+    });
+  }
+};
+
+const addDev = (FormData) => async (dispatch) => {
+  console.log(FormData);
+  try {
+    const { data } = await axios({
+      method: "post",
+      url: "http://127.0.0.1:3000/api/v1/developers",
+      data: FormData,
+    });
+    console.log(data);
+    dispatch({
+      type: types.ADD_DEVELOPER,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getDevelopers, addDev };
