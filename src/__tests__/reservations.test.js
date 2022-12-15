6d915a8946d07bf0868c01b9ebe9667e3f0d3605
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-15 22:28:43 +0500

    Add test case for the reservation page

---
+++ src/__tests__/reservations.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Reservations from '../components/reservations';
+import store from '../store/configStore';
+
+describe('Test Register page', () => {
+  const tree = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Reservations />
+      </BrowserRouter>
+    </Provider>,
+  );
+  it('renders correctly', () => {
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Developer list page has text Developers listing', () => {
+    expect(tree.findByText('reservations')).toBeTruthy();
+  });
+
+});
