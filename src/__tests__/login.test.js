de0a392740556378338ce863d6345f040781f7f9
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-15 22:01:04 +0500

    Fix linters error

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 // import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -15,7 +15,7 @@ describe('Test log in user page', () => {
     </Provider>,
   );
 
-  it('renders correctly', () => {  
+  it('renders correctly', () => {
     expect(tree).toMatchSnapshot();
   });
 
