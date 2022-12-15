63dc628e3888d14f186083c33c3231ce6f9a0113
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-15 22:28:19 +0500

    Add test case for the add developer page

---
+++ src/__tests__/addDeveloper.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import AddDeveloper from '../components/devs/addDeveloper';
+import store from '../store/configStore';
+
+describe('Test Register page', () => {
+  const tree = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <AddDeveloper />
+      </BrowserRouter>
+    </Provider>,
+  );
+  it('renders correctly', () => {
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Add Developer page has button Add Developer', () => {
+    expect(tree.findByText('Add Developer')).toBeTruthy();
+  });
+
+});
