c2b80aabf88ceb01b65b0a8ed62c2c44082987e7
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-12-16 08:19:16 +0100

    Add featured test for add developer page

--- src/__tests__/addDeveloper.test.js
+++ src/__tests__/addDeveloper.test.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import AddDeveloper from '../components/devs/addDeveloper';
-import store from '../store/configStore';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import AddDeveloper from "../components/devs/addDeveloper";
+import store from "../store/configStore";
+import userEvent from "@testing-library/user-event";
 
-describe('Test Register page', () => {
+describe("Test Register page", () => {
   const tree = render(
     <Provider store={store}>
       <BrowserRouter>
         <AddDeveloper />
       </BrowserRouter>
-    </Provider>,
+    </Provider>
   );
-  it('renders correctly', () => {
+  it("renders correctly", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('Add Developer page has button Add Developer', () => {
-    expect(tree.findByText('Add Developer')).toBeTruthy();
+  it("Add Developer page has button Add Developer", () => {
+    expect(tree.findByText("Add Developer")).toBeTruthy();
+  });
+
+  test("Add Developer page has name input ", () => {
+    render(<AddDeveloper />);
+    userEvent.type(screen.getByRole("textbox", { name: /title/i }));
+  });
+
+  it("Add Developer page has description input ", () => {
+    render(<AddDeveloper />);
+    userEvent.type(screen.getByRole("textbox", { name: /description/i }));
+  });
+
+  it("Add Developer page has salary input ", () => {
+    render(<AddDeveloper />);
+    userEvent.type(screen.getByRole("spinbutton", { name: /salary/i }));
+  });
+
+  it("Add Developer page has rating input ", () => {
+    render(<AddDeveloper />);
+    userEvent.type(screen.getByRole("spinbutton", { name: /rating/i }));
   });
 });
